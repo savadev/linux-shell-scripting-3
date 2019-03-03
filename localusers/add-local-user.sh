d3dfc3b31ecab10c68fab360851c6b28bbf1b9fe
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-03 18:00:41 -0500

    Add skeleton comments

--- localusers/add-local-user.sh
+++ localusers/add-local-user.sh
@@ -2,3 +2,25 @@
 
 # Prompts user for account information and creates new account.
 # NOTE: Script should be run from root account or using sudo.
+
+# Make sure a superuser is executing this script.
+
+# Prompt for login
+
+# Prompt for real name
+
+# Prompt for initial password
+
+# Create new user
+
+# !Check to see if account creation was successful
+
+# !Set initial password
+
+# !Check to see if password add was successful
+
+# !Force password change
+
+# Display username, password, and host (?) SEE MAN BASH
+
+
