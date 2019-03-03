56a78a3e61ffd0e9a1df8bc3cad7fdaac4ac5eb1
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-03 14:17:06 -0500

    Add more assignment and variable uses

--- localusers/luser-demo01.sh
+++ localusers/luser-demo01.sh
@@ -4,7 +4,25 @@
 echo 'Hello, World!'
 
 # Create a variable
-WORD='script'
+WORD='bird'
 
 # Display the value using the variable
 echo "$WORD"
+
+# Demonstrate single quote NON-expansion
+echo '$WORD'
+
+# Combine variable with hard-coded text
+echo "$WORD is the word"
+
+# Do it another way
+echo "${WORD} is the word"
+
+# But if you want to add something to the word...
+echo "${WORD}s are neat"
+
+# Create the ending to a word
+ENDING='ed'
+
+# Combine variables
+echo "This is ${WORD}${ENDING}"
