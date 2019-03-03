6dc77254fd19a5d36376ca9fcde72ea04d766483
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-03 16:56:01 -0500

    Create luser-demo03.sh
    Creation and comments

---
+++ localusers/luser-demo03.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Display the UID and username of the user executing this script.
+# Display if the user is the vagrant user or not.
+
+
+# Display the UID
+
+
+# Display a message if the UID does not match 1000
+
+
+# Display the username
+
+
+# Test if the command succeeded
+
+
+# Use a string test conditional
+
+
+# Test for string not equal
