d2b3e58c71990600121a16922e20aa310790a282
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-03 17:26:59 -0500

    Create luser-demo04.sh

---
+++ localusers/luser-demo04.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Creates an account on the local system.
+# You will be prompted for the account name and password.
+
+# Ask for username.
+
+
+# Ask for real name.
+
+
+# Ask for password.
+
+
+# Create user.
+
+
+# Set password for user.
+
+
+# Force password change.
