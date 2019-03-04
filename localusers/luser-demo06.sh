6bd9c353d909fd63915cff68265777c4a2a936b2
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-04 16:27:59 -0500

    Create luser-demo06.sh

---
+++ localusers/luser-demo06.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script generates a random password for each user specified on the command line.
+
+# Display what the user typed on the command line
+echo "You executed the command ${0}"
+
+# Display the path and filename of the script separately
+echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script"
+
+# Tell them how many arguments they passed
+
