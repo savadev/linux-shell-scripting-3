9880177c2adda8e3694048821c7847e0fd633322
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-04 18:35:37 -0500

    Create add-new-local-user.sh
    With skeleton comments

---
+++ localusers/add-new-local-user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Creates a new user with the username specified in the first argument.
+# Any additional arguments will be added as comments.
+# Usage add-new-local-user.sh USER_NAME [COMMENT]...
+
+# Enforce executing with superuser privileges
+
+
+# First argument is user name
+
+
+# Additional arguments are comments
+
+
+# Automatically generate a password
+
+
+# Display username, password, and host
