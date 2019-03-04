1e293136c13f4d4e4ed3a5a07305579cae509bf3
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-04 18:43:30 -0500

    Update add-new-local-user.sh
    Add enforcement of superid
    Add creation of account
    Add creation success check

--- localusers/add-new-local-user.sh
+++ localusers/add-new-local-user.sh
@@ -5,13 +5,28 @@
 # Usage add-new-local-user.sh USER_NAME [COMMENT]...
 
 # Enforce executing with superuser privileges
-
+if [[ ${UID} -ne 0 ]]
+then
+	echo 'Must create new accounts with superuser privileges. Exiting.'
+	exit 1
+fi
 
 # First argument is user name
-
+USERNAME=${1}
 
 # Additional arguments are comments
+shift
+COMMENTS="${*}
 
+# Create account
+useradd -c "${COMMENTS}" -m "${USERNAME}"
+
+# Make sure that worked
+if [[ $? -ne 0 ]]
+then
+	echo "Account ${USERNAME} creation failed."
+	exit 1
+fi
 
 # Automatically generate a password
 
