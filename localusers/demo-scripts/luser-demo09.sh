cffb8c8ef2b77b9b7d231a4b662e5973076c3088
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-08 20:03:31 -0500

    Show redirect implicit and explicit ways

--- localusers/demo-scripts/luser-demo09.sh
+++ localusers/demo-scripts/luser-demo09.sh
@@ -4,8 +4,18 @@
 
 # Read some text from a file the implicit way
 read X < /etc/centos-release
-echo ${X}
+echo "${X}"
 
 # Read some text the explicit way
 read X 0< /etc/centos-release
 echo "Should be the same text: ${X}"
+
+# Write some text to a file the implicit way
+echo "${UID}" > uid.txt
+cat uid.txt
+
+# Write some text to a file the explicit way
+echo "${UID}" 1> uid.txt
+cat uid.txt
+
+
