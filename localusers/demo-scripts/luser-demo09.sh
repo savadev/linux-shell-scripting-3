2014bfa18489e5e80f2e184a9bb21099f5ba57c8
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-08 20:44:02 -0500

    Update luser-demo09.sh

--- localusers/demo-scripts/luser-demo09.sh
+++ localusers/demo-scripts/luser-demo09.sh
@@ -2,6 +2,8 @@
 
 # This script shows explicit redirection to STDIN, STDOUT, and STDERR
 
+FILE="/tmp/data.both"
+
 # Read some text from a file the implicit way
 read X < /etc/centos-release
 echo "${X}"
@@ -18,4 +20,37 @@ cat uid.txt
 echo "${UID}" 1> uid.txt
 cat uid.txt
 
+# Redirect STDERR to a file using FD 2.
+ERR_FILE="/tmp/data.err"
+head -n3 /etc/passwd /fakefile 2> ${ERR_FILE}
 
+# Redirect STDOUT and STDERR to the same file
+head -n3 /etc/passwd /fakefile &> ${FILE}
+echo
+echo "Contents of ${FILE}:"
+cat ${FILE}
+
+# Redirect STDOUT and STDERR through a pipe.
+echo
+head -n3 /etc/passwd /fakefile |& cat -n
+
+# Send output to STDERR
+echo "Go to STDERR!" >&2
+
+# Discard STDOUT
+echo
+echo "Discarding STDOUT:"
+head -n3 /etc/passwd /fakefile > /dev/null
+
+# Discard STDERR
+echo
+echo "Discarding STDERR:"
+head -n3 /etc/passwd /fakefile 2> /dev/null
+
+# Discard both
+echo
+echo "Discarding both:"
+head -n3 /etc/passwd /fakefile &> /dev/null
+
+# Clean up
+rm ${FILE} ${ERR_FILE} &> /dev/null
