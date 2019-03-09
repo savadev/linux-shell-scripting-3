e9a807e140f8d5ae5a73eb10268a484b747dc22c
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-08 19:41:43 -0500

    Update luser-demo08.sh
    Show redirecting STDIN and appending STDOUT redirect

--- localusers/demo-scripts/luser-demo08.sh
+++ localusers/demo-scripts/luser-demo08.sh
@@ -5,3 +5,13 @@
 # Redirect STDOUT to a file.
 FILE="/tmp/data.txt"
 head -n1 /etc/passwd > ${FILE}
+
+# Redirect STDIN to a program
+read LINE < ${FILE}
+echo "LINE contains: ${LINE}"
+
+# Redirect STDOUT to a file, overwriting the information there.
+head -n3 /etc/passwd > ${FILE}
+
+# Redirect STDOUT to a file, appending to the file.
+echo date | sha256sum >> ${FILE}
