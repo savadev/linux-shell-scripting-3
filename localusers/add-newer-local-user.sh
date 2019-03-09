3038d7ef88ed8dd7e0910958e58e41a2a4a27d17
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-09 16:01:34 -0500

    Create add-newer-local-user.sh
    Include skeleton comments

---
+++ localusers/add-newer-local-user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script adds users to the local system and conforms to Linux program standard conventions.
+
+# Enforce execution with superuser privileges
+
+# Return usage statement if script is not called properly (with account name)
+
+# The first argument is the username
+
+# Any other arguments are comment text
+
+# Check to see if the creation succeeded
+
+# Automatically generate a password
+
+# Check to see if password addition succeeded
+
+# Set password to expire
+
+# Display username, password, and host
