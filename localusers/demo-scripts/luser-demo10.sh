d360eb5865a582fbe4ac22ce7d739b5490afd551
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-10 19:48:43 -0400

    Update luser-demo10.sh
    Remove file path from log file message
    Add backup_file function to backup file
    Check to see if backup successful and report success in log file

--- localusers/demo-scripts/luser-demo10.sh
+++ localusers/demo-scripts/luser-demo10.sh
@@ -5,21 +5,41 @@
 # Define the function here
 log()
 {
+  # This function sends a message to syslog and to standard output if VERBOSE IS TRUE.
   local MESSAGE="${@}"
   if [[ "${VERBOSE}" = 'true' ]]
   then
     echo "${MESSAGE}"
   fi
-  logger -t "${0}" "${MESSAGE}"
+  logger -t "$(basename ${0})" "${MESSAGE}"
 }
 
-function anotherlog
+function backup_file
 {
-  echo 'You called the other log function!'
+  # This function creates a backup of a file. Returns non-zero status on error
+
+  local FILE="${1}"
+
+  # Make sure the file exists
+  if [[ -f "${FILE}" ]]
+  then
+    local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+    log "Backing up ${FILE} to ${BACKUP_FILE}."
+
+    # The exit status of the function will be the exit status of the cp command
+    cp -p ${FILE} ${BACKUP_FILE}
+  else
+    # The file does not exist, so return non-zero exit status
+    return 1
+  fi
 }
 
 # Call the function here WITH NO PARENS
-log I passed this message to the function!
-VERBOSE='true'
-log I turned verbose on now!
-anotherlog
+readonly VERBOSE='true'
+backup_file ${1}
+if [[ "${?}" -eq 0 ]]
+then
+  log 'File backup successful'
+else
+  log 'File backup unsuccessful'
+fi
