e9d2694dd4212321916496df5b42362e4f30e459
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-10 18:11:26 -0400

    Update luser-demo09.sh
    Show the same first argument checking with a case statement

--- localusers/demo-scripts/luser-demo09.sh
+++ localusers/demo-scripts/luser-demo09.sh
@@ -3,15 +3,33 @@
 # This script demonstrates case statements.
 
 # Use an if statement to check for parameters
-if [[ "${1}" = 'start' ]]
-then
-	echo 'Starting'
-elif [[ "${1}" = 'stop' ]]
-then
-	echo 'Stopping'
-elif [[ "${1}" = 'status' ]]
-then
-	echo 'Status:'
-else
-	echo 'This is not a valid option'
-fi
+#if [[ "${1}" = 'start' ]]
+#then
+#	echo 'Starting'
+#elif [[ "${1}" = 'stop' ]]
+#then
+#	echo 'Stopping'
+#elif [[ "${1}" = 'status' ]]
+#then
+#	echo 'Status:'
+#else
+#	echo 'This is not a valid option' >&2
+#	exit 1
+#fi
+
+# Do the same thing with a case statement
+case "${1}" in
+	start)
+		echo 'Starting'
+		;;
+	stop)
+		echo 'Stopping'
+		;;
+	status)
+		echo 'Status:'
+		;;
+	*)
+		echo 'Supply a valid option' >&2
+		exit 1
+		;;
+esac
