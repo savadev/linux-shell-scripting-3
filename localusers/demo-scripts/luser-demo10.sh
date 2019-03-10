7852149b9ab1c8410c420ed549dc3f9156f82aff
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-10 18:24:06 -0400

    Create luser-demo10.sh
    Define and call a basic function

---
+++ localusers/demo-scripts/luser-demo10.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script shows how to write functions.
+
+# Define the function here
+log()
+{
+  echo 'You called the log function!'
+}
+
+# Call the function here WITH NO PARENS
+log
