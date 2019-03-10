323cb2e759e7f8009783e1a4daab494e91dc56f9
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-10 18:01:18 -0400

    Update luser-demo09.sh
    Show use of if-statement to check for specific values of first argument

--- localusers/demo-scripts/luser-demo09.sh
+++ localusers/demo-scripts/luser-demo09.sh
@@ -2,4 +2,16 @@
 
 # This script demonstrates case statements.
 
-
+# Use an if statement to check for parameters
+if [[ "${1}" = 'start' ]]
+then
+	echo 'Starting'
+elif [[ "${1}" = 'stop' ]]
+then
+	echo 'Stopping'
+elif [[ "${1}" = 'status' ]]
+then
+	echo 'Status:'
+else
+	echo 'This is not a valid option'
+fi
