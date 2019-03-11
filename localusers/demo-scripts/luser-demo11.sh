566161fd7c571a8b410ba00fa516ff87eb166be9
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-10 20:29:26 -0400

    Update luser-demo11.sh
    Set default length
    Use getopts in while loop to process options and arguments

--- localusers/demo-scripts/luser-demo11.sh
+++ localusers/demo-scripts/luser-demo11.sh
@@ -4,3 +4,22 @@
 # - Password length can be set with -l
 # - Special character can be added with -s
 # - Verbose mode can be enabled with -v
+
+# Set a default length
+LENGTH=48
+
+while getopts vl:s OPTION
+do
+  case "${OPTION}" in
+    v)
+      VERBOSE='true'
+      echo 'Verbose mode on'
+      ;;
+    l) LENGTH="${OPTARG}" ;;
+    s) USE_SPECIAL='true' ;;
+    ?)
+      echo 'Invalid option.' >&2
+      exit 1
+      ;;
+  esac
+done
