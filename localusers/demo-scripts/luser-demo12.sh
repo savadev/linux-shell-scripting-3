e2d06ae5df590bc59981ae2e4ec08738c504e087
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-23 12:04:01 -0400

    Add sorting and unique port finding

--- localusers/demo-scripts/luser-demo12.sh
+++ localusers/demo-scripts/luser-demo12.sh
@@ -3,4 +3,4 @@
 # This script shows the open network ports on a system.
 # Use -4 as an argument to limit to tcpv4 ports
 
-netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'
+netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}' | sort -n -u
