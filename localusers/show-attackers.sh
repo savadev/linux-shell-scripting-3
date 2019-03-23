ee3b38c164afdbe629038038b822f7831bbcb9b9
Stephen Mihalko <stephen.mihalko@gmail.com>
2019-03-23 12:30:36 -0400

    Update show-attackers.sh
    Add display usage function
    Check for arguments provided
    Check for file exists

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -2,3 +2,19 @@
 
 # This script reads a log file of ipv4 addresses from which failed login
 # attempts were initiated and prints them in the form "Count,IP,Location"
+
+usage()
+{
+  echo 'This is only a test.' >&2
+  exit 1
+}
+
+if [[ ${#} -eq 0 ]]
+then
+  usage
+elif [[ ! -f ${1} ]]
+then
+  echo "File ${1} cannot be read." >&2
+fi
+
+
